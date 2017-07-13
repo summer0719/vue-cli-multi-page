ba149c8c1aac848032dcf9715bf1fe1b3b936b89
summer000 <1468609385@qq.com>
2017-07-13 16:46:05 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,15 +2,16 @@
 var path = require('path')
 
 module.exports = {
-  build: {
-    index: path.resolve(__dirname, 'dist/index.html'),
-    assetsRoot: path.resolve(__dirname, 'dist'),
-    assetsSubDirectory: 'static',
-    assetsPublicPath: process.env.NODE_ENV === 'production' ? '../' : '/',
-    productionSourceMap: true
-  },
-  dev: {
-    port: 8083,
-    proxyTable: {}
-  }
+    build: {
+        index: path.resolve(__dirname, 'dist/index.html'),
+        assetsRoot: path.resolve(__dirname, 'dist'),
+        assetsSubDirectory: 'static',
+        assetsPublicPath: process.env.NODE_ENV === 'production' ? '/adstatic/adv2/dist' : '/',
+        productionSourceMap: true
+    },
+    dev: {
+        port: 8112,
+        proxyTable: { '/adv2': 'http://beta.ad.immomo.com:8181', '/ad': 'http://beta.ad.immomo.com:8181'}
+    }
+
 }
