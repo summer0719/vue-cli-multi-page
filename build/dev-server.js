fcce02c08aa50fc7450088424afa33725b66b14f
summer000 <1468609385@qq.com>
2017-07-13 16:43:26 +0800

    Update dev-server.js

--- build/dev-server.js
+++ build/dev-server.js
@@ -2,44 +2,69 @@ var path = require('path')
 var express = require('express')
 var webpack = require('webpack')
 var config = require('../config')
+var opn = require('opn')
 var proxyMiddleware = require('http-proxy-middleware')
-var webpackConfig = process.env.NODE_ENV === 'testing'
-  ? require('./webpack.prod.conf')
-  : require('./webpack.dev.conf')
+var webpackConfig = process.env.NODE_ENV === 'testing' ? require('./webpack.prod.conf') : require('./webpack.dev.conf')
 
 // default port where dev server listens for incoming traffic
 var port = process.env.PORT || config.dev.port
-// Define HTTP proxies to your custom API backend
-// https://github.com/chimurai/http-proxy-middleware
+    // Define HTTP proxies to your custom API backend
+    // https://github.com/chimurai/http-proxy-middleware
 var proxyTable = config.dev.proxyTable
 
 var app = express()
+    // 配置假数据 todo
+var appData = require('../mock/data.json');
+var apiRoute = express.Router();
+apiRoute.get('/nav', function(req, res) {
+    res.json(appData);
+});
+apiRoute.get('/ideaone', function(req, res) {
+    res.json(appData);
+});
+apiRoute.get('/idealist', function(req, res) {
+    res.json(appData);
+});
+
+// 所有接口都在/api目录（可以自我定义）下，访问通过  /apitest/seller
+app.use('/api', apiRoute)
+
+//测试数据接口开始
+var appDataTwo = require('../mock/datatwo.json');
+apiRoute.get('/putData', function(req, res) {
+    res.json(appDataTwo);
+});
+apiRoute.get('/statis/index', function(req, res) {
+    res.json(appDataTwo);
+});
+//结束
+
 var compiler = webpack(webpackConfig)
 
 var devMiddleware = require('webpack-dev-middleware')(compiler, {
-  publicPath: webpackConfig.output.publicPath,
-  stats: {
-    colors: true,
-    chunks: false
-  }
+    publicPath: webpackConfig.output.publicPath,
+    stats: {
+        colors: true,
+        chunks: false
+    }
 })
 
 var hotMiddleware = require('webpack-hot-middleware')(compiler)
-// force page reload when html-webpack-plugin template changes
-compiler.plugin('compilation', function (compilation) {
-  compilation.plugin('html-webpack-plugin-after-emit', function (data, cb) {
-    hotMiddleware.publish({ action: 'reload' })
-    cb()
-  })
+    // force page reload when html-webpack-plugin template changes
+compiler.plugin('compilation', function(compilation) {
+    compilation.plugin('html-webpack-plugin-after-emit', function(data, cb) {
+        // hotMiddleware.publish({ action: 'reload' })
+        cb()
+    })
 })
 
 // proxy api requests
-Object.keys(proxyTable).forEach(function (context) {
-  var options = proxyTable[context]
-  if (typeof options === 'string') {
-    options = { target: options }
-  }
-  app.use(proxyMiddleware(context, options))
+Object.keys(proxyTable).forEach(function(context) {
+    var options = proxyTable[context]
+    if (typeof options === 'string') {
+        options = { target: options, changeOrigin: true }
+    }
+    app.use(proxyMiddleware(context, options))
 })
 
 // handle fallback for HTML5 history API
@@ -55,11 +80,15 @@ app.use(hotMiddleware)
 // serve pure static assets
 var staticPath = path.join(config.build.assetsPublicPath, config.build.assetsSubDirectory)
 app.use(staticPath, express.static('./static'))
-
-module.exports = app.listen(port, function (err) {
-  if (err) {
-    console.log(err)
-    return
-  }
-  console.log('Listening at http://localhost:' + port + '\n')
+var uri = 'http://localhost:' + port
+module.exports = app.listen(port, function(err) {
+    if (err) {
+        console.log(err)
+        return
+    }
+    console.log('Listening at http://localhost:' + port + '\n')
+        // when env is testing, don't need open it
+    if (process.env.NODE_ENV !== 'testing') {
+        opn(uri)
+    }
 })
